97d4d729c707068b168ea246aae3a61f96da28ab
Matias Labra <gmlabra@gmail.com>
2020-06-07 22:41:57 +1000

    refactor: exposing graphql / algolia functions

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,4 @@
-'use strict';
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const isProd = functions.config().runtime.env === 'production';
-const serviceAccount = functions.config().service_account;
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://myvocab-fa620.firebaseio.com',
-});
-
-const server = require('./graphql/server');
-const corsOptions = {
-  origin: isProd ? 'https://myvocab-fa620.web.app/' : 'http://localhost:3000',
-  credentials: true,
-};
-// :todo: set cors origin back;
-const app = server.createHandler({
-  cors: corsOptions,
-});
-// Expose Express API as a single Cloud Function:
-exports.graphql = functions.https.onRequest(app);
+const graphql = require('./graphql');
+const algolia = require('./algolia');
+exports.server = graphql;
+exports.algolia = algolia;
